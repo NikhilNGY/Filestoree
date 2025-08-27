e606899e1ab9110a90b572ac212e2d22068ee545
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-08-27 13:17:57 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -5,41 +5,12 @@ from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-HELP_TXT = f"""Any Issues Movie Files Contact Owner
-
-🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href=https://t.me/Nikhil5757h>Owner</a>"""
-
-ABOUT_TXT = f"""Any Issues Movie Files Contact Owner
-
-🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href=https://t.me/Nikhil5757h>Owner</a>"""
-
 START_TXT = f"""Friends.......🖤 
 We have already lost many channels due to copyright... So join us by giving your support, cooperation and blessings to this new channel of ours 🙏🙏"""
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if query.data == "help":
-        buttons = [[
-            InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='start')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=HELP_TXT,
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
-    if query.data == "about":
-        buttons = [[
-            InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='start')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-
-        await query.message.edit_text(
-            text=ABOUT_TXT,
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
     if query.data == "start":
         reply_markup = InlineKeyboardMarkup(
 
